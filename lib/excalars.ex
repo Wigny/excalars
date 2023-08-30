f7f1a3e6e82709dea3eee905132943042ad2a0e7
Wígny Almeida <wignybora@gmail.com>
2023-08-30 11:45:46 -0400

    Git init

---
+++ lib/excalars.ex
@@ -0,0 +1,18 @@
+defmodule Excalars do
+  @moduledoc """
+  Documentation for `Excalars`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Excalars.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excalar_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcalarsTest do
+  use ExUnit.Case
+  doctest Excalars
+
+  test "greets the world" do
+    assert Excalars.hello() == :world
+  end
+end
